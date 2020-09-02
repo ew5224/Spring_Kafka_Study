e019a72d7e0a30a16d406bcc2db82a688aea6730
김용우 <roddie@gim-yong-uui-MacBookPro.local>
2020-09-02 21:28:32 +0900

    Day2 Additional Study

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,49 @@
+package hello.hellospring.controller;
+
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    /// 여러 객체에서 MemberSerivce를 쓰고 싶어. 근데 하나만 필요해 공용으로 쓰는
+    ///private final MemberService memberService = new MemberService();
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createFrom(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
+    }
+}
